f3bfbc9a0bff32d9bc4c0e57f9c66a3e92d847c4
sbae13 <301024829@cencol.ca>
2019-06-25 10:44:45 -0400

    Added CreateJS

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,8 +1,38 @@
 (function(){    // function run by itself - anonymous function?
-    function Init(){
-        console.log("Initialization Start");
 
-        let x: number = 10;
+    // Global Game Variables
+    let canvas = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel:createjs.Text;
+
+    function Init():void{   // funcion name():return type
+        console.log("Initialization start");
+        Start();        
+    }
+
+    function Start(){
+        console.log("Starting Application");
+
+        // Initialize CreateJS
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 FPS (Frames per second)
+        createjs.Ticker.on("tick", Update);    // add eventlistner
+        Main();
+    }
+
+    function Update(){
+        helloLabel.rotation += 5;
+        stage.update(); // clear the stage and re-draw
+    }
+
+    function Main(){
+        console.log("Game Start...");
+
+        helloLabel = new createjs.Text("Hello World", "40px Consolate", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+
+        stage.addChild(helloLabel);
     }
 
     window.onload = Init;
